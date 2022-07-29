30d9f67c89fadf486ca2d26bcbf6488097c071b8
deepaksemwal121 <deepaksemwal121@gmail.com>
2022-07-29 12:06:10 +0530

    Added Login page

--- src/Screens/AuthScreens/Login.js
+++ src/Screens/AuthScreens/Login.js
@@ -1,17 +1,117 @@
-import React from 'react'
+import { useState } from "react";
+import {BsArrowLeft} from "react-icons/bs";
+
+
 
 const Login = () => {
+
+const [hide , setHide] = useState(true);
+const [resend , setresend] = useState(true);
+const [otp,setOTP]= useState(new Array(4).fill(""));
+
+const handleChange = (e , i) => {
+
+    if(isNaN(e.target.value)) return false;
+
+    setOTP([...otp.map((data,idx) => (idx === i) ? e.target.value:data)]);
+
+    //Change Focus to next Box 
+
+    if(e.target.nextSibling){
+        e.target.nextSibling.focus();
+    }
+
+    if (e.inputType === 'deleteContentBackward' && e.target.value === '') {
+        e.target.lastChild.focus();
+      }
+
+
+}
+
+
   return (
-    <div className="flex justify-center item-center bg-[#141414] h-screen">
+    <div className='flex justify-center items-center w-100 bg-[#141414] h-screen'>
         <div>
-            <div> -- Go Back</div>
-            <div>
-                <div>Login form </div>
-                <div> content</div>
+        <div className='text-white flex space-x-4 items-center pb-[21px]'><BsArrowLeft size={25} /><span className="text-[20px]">Go back</span></div>
+            <div className='rounded-[20px] shadow-lg space-x-10 flex p-[115px] bg-[#282828]'>
+
+                {/* Phone Number Input */}
+
+                <div className={`w-[50%] ${hide ?"":"hidden"}`}>
+                    <div> 
+                    <h3 className="text-white font-[500] text-[36px]">Welcome to Amuzi</h3>
+                    <p className="text-[#828282] text-[18px] w-[411px]">Register on Amuzi and Sign in to your existing account via Mobile Number</p>
+                    </div>
+                    <div className="pt-[41px] pb-[40px]">
+                        <label className="text-white">Enter Your Mobile Number</label>
+                        <div className="border-b-[1px] border-[#F5F5F5] w-[380px] flex">
+                            <span className="p-2 text-[18px] text-[#f5f5f5]" >+91-</span>
+                            <input type="text" className=" text-[18px] p-2 bg-transparent focus: ring-0 focus:outline-none text-[#f5f5f5]" maxLength="10" />
+                        </div>
+                        <div>
+                        <span className="text-red-500 py-6"></span>
+                        </div>
+                        <div className="flex items-start py-[40.99px] space-x-3 w-[340px]">
+                            <div className="checkbox-container rounded-lg">
+                            <input id="checkb" type="checkbox" className=" w-[24px] h-[24px]"/>
+                            </div>
+                            <p className="text-[#FFFFFF] text-[18px]">I have read and hereby accept the <strong>Term
+                                of use</strong> and <strong>Privacy note</strong></p>
+                        </div>
+                        <div className="pt-[74px] w-[380px] text-center">
+                            <div className="bg-[#141414] px-[18px] py-[16px] rounded-[16px] text-[#f5f5f5] text-[16px] font-[700] hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 " onClick={()=> setHide(false)}> Continue</div>
+                        </div>
+                    </div>
+                     </div>
+
+                     {/* OTP Box */}
+
+                     <div className={`w-[50%] ${!hide ?"":"hidden"}`}>
+                    <div> 
+                    <h3 className="text-white font-[500] text-[36px]">Phone Number Verification</h3>
+                    <p className="text-[#828282] text-[18px] w-[411px]">Enter OTP For Registration</p>
+                    </div>
+                    <div className="pt-[41px] pb-[40px]">
+                        <label className="text-white">Enter OTP To Verify</label>
+                        <div className="space-x-2 w-[380px] flex mt-6">
+                            {otp.map((item,index)=>{
+                                return <input className="w-12 h-12 text-center rounded-lg bg-[#141414] text-white p-1" name="otp" maxLength={1} key={index} onChange={(e)=> handleChange(e , index)} onFocus={ e => e.target.select()} type="text" />
+                            })}
+                           
+                        </div>
+                        <div>
+                            {/*Errror Throw Message  */}
+                        <span className="text-red-500 py-6"></span>
+                        </div>
+                        <div className="flex items-start py-[40.99px] space-x-3 w-[340px]">
+                            <button className={`text-[#FFFFFF] text-[18px]`} >Resend OTP <span> in 45 sec </span></button>
+                        </div>
+                        <div className="pt-[74px] w-[380px] text-center">
+                            <div className="bg-[#141414] px-[18px] py-[16px] rounded-[16px] text-[#f5f5f5] text-[16px] font-[700] hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ">Verify OTP</div>
+                        </div>
+                    </div>
+                     </div>
+
+                     {/* Graphics Styles Banner */}
+                <div className="w-[50%] ">
+                    <div className=" flex items-baseline justify-end space-x-2">
+                        <p className="text-[#f5f5f5] font-[700] text-[31px]">Amuzi </p>
+                        <div className="h-4 w-4 rounded-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"></div> 
+                    </div>
+
+                    <div className="rounded-lg pt-[84px] bg-blend-multiply relative">
+                        <div className="bg-gradient-to-b w-full h-full from-transparent z-20 to-[#141414] absolute top-0 rounded-b-xl"></div>
+                        <img className="rounded-xl bg-blend-lighten" src="https://i.guim.co.uk/img/media/62b645060ff895a9f3a19c2056bd40e474512bf3/0_204_4189_2514/master/4189.jpg?width=620&quality=85&fit=max&s=2784fe2bde0b2fc54ebb8eefd23ba238" alt ="login"/>
+                    </div>
+                    <div className="absolute p-2 z-30 top-[589px]">
+                   <h3 className="w-[450px] font-[700] text-[48px] text-white leading-[50px] ">Enjoy Live streaming of your favourite sports</h3> 
+                    </div>
+                </div>
             </div>
         </div>
     </div>
   )
 }
+
 
 export default Login
--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import banner from "../assets/banner.png"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <Navbar />
+      <div className='bg-[#141414] w-full h-screen'>
+        {/* Banner Section  */}
+        <div className='flex justify-between'>
+          <div className='flex flex-col pt-[198px] pl-[70px]  w-[581px]'>
+            <h3 className='text-[32px] font-[700] text-white leading-[36px]'>Mumbai Indians Vs Chennai Superkings</h3>
+            <p className='text-[#c9c8c8] text-[18px] font-[500] leading-[36px]' >Indian Premier League 2022</p>
+            <p className='text-[#a4a2a2] leading-[36px] text-[16px]'>Catch the Live matches of Indian Premier League only on Amuzi</p>
+            <button className='mt-[70px] w-[160px] rounded-lg px-[30px] py-[10px] text-[18px] bg-white'>Watch Live</button>
+            <p className='text-[#a4a4a4] leading-8 text-[14px]'>Register Now to Watch The upcoming live matches</p>
+          </div>
+          <div className='' >
+            <img src={banner} alt= "banner" />
+          </div>
+        </div>
+        <div className='px-[70px]'>
+          <h3 className='text-[30px] leading-[30px] font-[600] text-white'>Upcoming Events</h3>
+          <div>
+            1
+          </div> <div>
+            1
+          </div> <div>
+            1
+          </div> <div>
+            1
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
